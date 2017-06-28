b4a6842091caf24ad3d81036cf436a54a182174f
Gabriel <g4briel.4lves@gmail.com>
2017-06-28 13:50:35 -0300

    Adicionado evento fechar janela

--- src/GameLoop.cpp
+++ src/GameLoop.cpp
@@ -4,7 +4,13 @@ int main(){
   sf::RenderWindow window(sf::VideoMode(200, 200), "Roguelike");
 
   while(window.isOpen()){
-
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            window.close();
+    }
+    
     window.clear();
     window.display();
   }
