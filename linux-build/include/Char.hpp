debed3034a59ed26caa40853a14b0b061a04bd9e
Gabriel <g4briel.4lves@gmail.com>
2018-01-15 19:20:42 -0200

    Update Char.hpp

--- linux-build/include/Char.hpp
+++ linux-build/include/Char.hpp
@@ -7,38 +7,38 @@
 
 class Char : public Object{
 protected:
-    // array of texture IDs
-    int m_textureIDs[static_cast<int>(ANIMATION_STATE::COUNT)];
+  // array of texture IDs
+  int m_textureIDs[static_cast<int>(ANIMATION_STATE::COUNT)];
 
-    // current texture
-    int m_currentTexID;
+  // current texture
+  int m_currentTexID;
 
-    // base HP
-    double m_charMaxHp;
+  // base HP
+  double m_charMaxHp;
 
-	  // current HP
-	  double m_charHp;
+  // current HP
+  double m_charHp;
 
-    // base damage
-    double m_charBaseDmg;
+  // base damage
+  double m_charBaseDmg;
 
-    // base defense
-    double m_charBaseDef;
+  // base defense
+  double m_charBaseDef;
 
-    // character's base movement speed
-    int m_charSpeed;
+  // character's base movement speed
+  int m_charSpeed;
 
-    // character's current velocity
-    sf::Vector2f m_velocity;
+  // character's current velocity
+  sf::Vector2f m_velocity;
 
  public:
   Char();
 
   double getMaxHP() const {return m_charMaxHp;};
-	double getHP() const {return m_charHp;};
-	void setHP(double);
+  double getHP() const {return m_charHp;};
+  void setHP(double);
 
-	double getBaseDmg() const {return m_charBaseDmg;};
+  double getBaseDmg() const {return m_charBaseDmg;};
   void setBaseDmg(double);
   double getBaseDef() const {return m_charBaseDef;};
   void setBaseDef(double);
